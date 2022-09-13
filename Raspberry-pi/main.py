fa8392d5eb06118db3c117c7e231d1d9e87869dd
Ye0ngJae <kimjh38012@outlook.com>
2022-09-14 01:56:33 +0900

    Update main.py

--- Raspberry-pi/main.py
+++ Raspberry-pi/main.py
@@ -4,35 +4,39 @@ import os
 import time
 import threading
 from pyfirmata import Arduino, util
+#import picamera
 
-url  = "https://api.plebea.site/accident"
+info_url  = "https://api.plebea.site/accident"
+hardware_url = "https://api.plebea.site/hardware"
 board = Arduino("/dev/ttyACM0")
 
+#camera = picamera.PiCamera()
+#camera.resolution = (1920, 1080)
+
 def get_hardware_info():
-    result = os.system('archey')
+    #result = os.system('archey')
     
     info = { #example
         "cpu": "Raspberry Pi 3 Model B",
-        "ram": "1 GB",
-        "gpu": "Broadcom BCM2837",
+        "ram": "1 GB / test",
         "os": "Raspbian GNU/Linux 10 (buster)",
         "kernel": "Linux 4.19.97-v7+",
         "uptime": "1 day, 1 hour, 1 minute",
-        "shell": "bash 5.0.3",
-        "resolution": "1920x1080",
-        "de": "Raspberry Pi Configuration"
     }
     
-    json_data = json.loads(result)
+    json_data = json.loads(info)
     
-    requests.post()
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    requests.post(hardware_url, data=json_data, headers=headers)
 
 def get_sensor_info():
-    print("hi")
+    
+    requests.post()
+    
+def get_camera():
+    #camera.capture('image.png')
 
-while(1): #5 min loop
-    get_hardware_info()
-    time.sleep(300)
+
     
     
 
